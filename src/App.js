6b428d8ac97d483d788f71d70443a17c6c642b72
Yaswanth <yxpanguluri@shockers.wichita.edu>
2023-12-29 19:55:08 -0600

    Router Module is implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 // src/components/App.js
 
-import React from 'react';
-import HomePage from './components/HomePage';
-import './App.css';
+import React from "react";
+import HomePage from "./components/HomePage";
+import Ats from "./components/Ats";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/ats" element={<Ats />} />
+        </Routes>
+      </Router>
+
       <p>© 2023 Yaswanth Panguluri</p>
     </div>
   );
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,20 +1,11 @@
-import React from 'react';
-import '../styles/Yaswanth.css';
-import Ats from './Ats';
+import React from "react";
+
+import "../styles/HomePage.css";
 
 const HomePage = () => {
   return (
     <div className="container mt-4">
       <h1 className="text-center">Yaswanth Panguluri </h1>
-
-
-      <div className="row">
-        <div className="col-md-12">
-          <Ats />
-        </div>
-      </div>
-
-
     </div>
   );
 };
